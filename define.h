5cfdc216ad3ddaab328629c03a11cddcfbc77459
nguyen trung duc <nguyentrungduc08@gmail.com>
2018-06-02 22:46:35 +0700

    test build + code

---
+++ define.h
@@ -0,0 +1,13 @@
+#ifndef DEFINE_H
+#define DEFINE_H
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <thread>
+
+#define rep(i,n) 	for(int i = 0; i < (int) n; ++i)
+#define X 			first
+#define Y 			second
+
+#endif
---
+++ smartPoiter/main.cpp
@@ -0,0 +1,34 @@
+#include "../define.h"
+
+using namespace std;
+
+float n, s, v, vs;
+
+float tRest = 0.0;
+
+int main(){
+	freopen("test.txt", "r", stdin);
+
+	cin >> n >> s >> v >> vs;
+
+	//cout << "max: " << s/vs << endl;
+	float maxT = s/vs;
+
+	float x;
+	while (n>4){
+		x = (v*v*s + v*vs*s) / (v*v +2*v*vs - vs*vs);
+		tRest += x/v + (s-x)/vs;
+	       	s -= vs*x/v + (s-x);	
+		n-=4;
+	}
+
+	if (n>0){
+		tRest += s/v;	
+	}	
+
+	if (tRest < maxT)
+		cout << tRest << endl;
+	else 
+		cout << maxT << endl;
+	return 0;
+}
